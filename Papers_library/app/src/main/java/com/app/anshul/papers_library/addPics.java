d265dcfb02f341a12136a0ce9e777690fa702312
anshsvnit <anshsvnit@gmail.com>
2016-12-23 03:44:01 +0530

    Upload pics from gallery for PDF

--- Papers_library/app/src/main/java/com/app/anshul/papers_library/addPics.java
+++ Papers_library/app/src/main/java/com/app/anshul/papers_library/addPics.java
@@ -2,12 +2,22 @@ package com.app.anshul.papers_library;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.content.ClipData;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by anshul on 21/12/16.
@@ -19,6 +29,13 @@ public class addPics extends AppCompatActivity {
 
     TextView courseSelected,yearSelected,deptSelected,semSelected;
     LinearLayout layoutcourse,layoutyear,layoutdept,layoutsem;
+    Button upload,takePics;
+    private static final int RESULT_LOAD_IMAGE=1;
+    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+    ClipData.Item item;
+    String imageEncoded;
+    List<String> imagesEncodedList;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,7 @@ public class addPics extends AppCompatActivity {
         initialize();
         Bundle bundle = getIntent().getExtras().getBundle("Values");
         int selection = bundle.getInt("Selection");
+        upload = (Button) findViewById( R.id.galleryupload);
 
          if (selection == 1){
             setup1styearTview(bundle);
@@ -37,8 +55,60 @@ public class addPics extends AppCompatActivity {
 
              setupRestYearTview(bundle);
          }
+
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), RESULT_LOAD_IMAGE);
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+        if (resultCode == RESULT_OK) {
+            if(requestCode == RESULT_LOAD_IMAGE){
+                if (data.getClipData() != null) {
+                    ClipData mClipData = data.getClipData();
+                    imagesEncodedList = new ArrayList<String>();
+
+                    for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                        item = mClipData.getItemAt(i);
+                        uri = item.getUri();
+                        mArrayUri.add(uri);
+                        Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+
+                        cursor.moveToFirst();
+
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        imageEncoded = cursor.getString(columnIndex);
+                        Log.v("clip", imageEncoded);
+
+                        imagesEncodedList.add(imageEncoded);
+                        cursor.close();
+
+                    }
+                    //Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                }
+
+            }
+        }
+
+    }
     public void initialize(){
         courseSelected = (TextView) findViewById(R.id.courseselected);
         yearSelected =(TextView) findViewById(R.id.yearSelected);
